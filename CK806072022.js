ea48f6df26e5d69ff188f4102a2a5bc45e9301e5
blueurchin4u <harris.jr.zayne.d@gmail.com>
2022-06-07 22:45:19 -0500

    Even or Odd 8kyu solution

---
+++ CK806072022.js
@@ -0,0 +1,17 @@
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+/*
+1. Make a function
+2. Make a conditional that checks to see if number is even or odd - an even number is divisible by two with a remainder of  0
+3. Return
+*/
+
+
+function even_or_odd(number) {
+    if (number % 2 === 0) {
+      return 'Even'
+    } else {
+      return 'Odd'
+    }
+  }

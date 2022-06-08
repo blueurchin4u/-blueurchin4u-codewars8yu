59058d3e1629a78d0b2cc2fe5abdb5d702339a09
blueurchin4u <harris.jr.zayne.d@gmail.com>
2022-06-07 23:42:37 -0500

    Added Convert Number to a String 8kyu Kata

---
+++ CK806072022no3.js
@@ -0,0 +1,13 @@
+/* Convert a Number to a String!
+
+Instructions: We need a function that can transform a number into a string.  What ways of achieving this do you know?
+
+1. Create a function
+2. Use string method .toString() with parameter
+*/
+
+function numberToString(num) {
+    return num.toString()
+  }
+  
+  numberToString(9)

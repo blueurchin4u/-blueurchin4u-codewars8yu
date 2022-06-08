abc429a1eae8f00e5c2c7f2525103b04faa67d7d
blueurchin4u <harris.jr.zayne.d@gmail.com>
2022-06-08 10:07:41 -0500

    Added codewars 8kyu solution, changed file names of 2 previous files to show progress

---
+++ CK806082022no2.js
@@ -0,0 +1,17 @@
+/*
+Reversed Strings
+
+Complete the solution so that it reverses the string passed into it.
+
+1. Create function
+2. Create variable that is assigned a chained string split method, reverse and join array methods
+3. Return the variable
+
+*/
+
+function solution(str){
+    const reversedString = str.split('').reverse('').join('')
+    return reversedString
+  }
+  
+  

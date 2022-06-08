b4fe6692cdf22ee1c76d67d547ee5f5eda70a33d
blueurchin4u <harris.jr.zayne.d@gmail.com>
2022-06-07 23:33:13 -0500

    Added Opposite Number 8kyu Kata

---
+++ CK806072022-2.js
@@ -0,0 +1,10 @@
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+1. Create a function. 
+2. Return value * a negative, as a positive x negative = a negative, and a negative x negative = a positive.
+*/
+
+function opposite(number) {
+    return -(number) 
+   }
